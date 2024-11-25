2b4876f0ba3d7599b39d8c5951afcb52958a644d
Sikirulahi <kareemsikiru2018@gmail.com>
2024-11-25 07:53:50 +0100

    The tutorial on Langchain

---
+++ chatbot/locallama.py
@@ -0,0 +1,34 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+
+# Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You're a helpful assistant, please respond to the user queries"),
+        ("user", "Question : {question}")
+    ]
+)
+
+# Streamlit framework
+
+st.title("LangChain demo with OPENAI API")
+input_text = st.text_input("Search the topic you want")
+
+llm = Ollama(model='gpt-3.5-turbo')
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
